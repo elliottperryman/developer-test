117174fe9eee96c4273fc2a0504bdbb3870b2731
elliott <elliott.perryman@hey.com>
2023-10-27 19:01:32 +0200

    working on frontend

--- solution/frontend/main.py
+++ solution/frontend/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
 from jsonschema import validate
 from solution.schemas import empire_schema
 from solution.solve import solve
@@ -7,12 +8,14 @@ from solution.empire import Empire
 from typing import Dict
 
 app = FastAPI()
+templates = Jinja2Templates(directory="frontend")
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(request:Request):
+    print(request)
+    return templates.TemplateResponse('page.html', context={'request':request})
 
-@app.post("/solve/")
+@app.post("/")
 async def create_item(data:Dict):
     print(data)
     validate(data, schema=empire_schema)
